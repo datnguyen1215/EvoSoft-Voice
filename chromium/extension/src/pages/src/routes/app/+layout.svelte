283c268e310ca86c5530988058472839551c964f
Dat Nguyen <datnguyen1215@gmail.com>
2024-03-03 13:52:58 -0800

    implement simple page.

---
+++ chromium/extension/src/pages/src/routes/app/+layout.svelte
@@ -0,0 +1,8 @@
+<script>
+  import SideNav from './components/SideNav.svelte';
+</script>
+
+<div class="flex h-full">
+  <SideNav />
+  <slot />
+</div>
---
+++ chromium/extension/src/pages/src/routes/app/components/SideNav.svelte
@@ -0,0 +1,40 @@
+<script>
+  import SignalIcon from '$lib/components/icons/Signal.svelte';
+  import HomeIcon from '$lib/components/icons/Home.svelte';
+  import SettingsIcon from '$lib/components/icons/Settings.svelte';
+
+  const menus = [
+    {
+      name: 'Home',
+      icon: HomeIcon,
+      link: '/'
+    },
+    {
+      name: 'Settings',
+      icon: SettingsIcon,
+      link: '/settings'
+    }
+  ];
+</script>
+
+<div class="w-fit max-w-[300px] min-w-[150px] py-5 px-5 shadow-md">
+  <div class="flex pb-5">
+    <SignalIcon class="w-10 h-10 text-red-700" />
+    <h1 class="text-2xl font-bold ml-2 text-indigo-700 m-0 p-0">EvoSoft Voice</h1>
+  </div>
+
+  <ul class="flex flex-col">
+    {#each menus as menu}
+      <a href={menu.link}>
+        <li class="hover:bg-gray-200 my-2 px-5 rounded-lg py-2">
+          <div class="flex items-center space-x-2">
+            {#if menu.icon}
+              <svelte:component this={menu.icon} class="w-5 h-5" />
+            {/if}
+            <span>{menu.name}</span>
+          </div>
+        </li>
+      </a>
+    {/each}
+  </ul>
+</div>
