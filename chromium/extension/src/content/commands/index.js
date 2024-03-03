af5725fd40989bf38aad3c6c5d7c8ed9d92cdf06
Dat Nguyen <datnguyen1215@gmail.com>
2024-03-03 08:41:11 -0800

    Toggle ui display for voice commands.

--- chromium/extension/src/content/commands/index.js
+++ chromium/extension/src/content/commands/index.js
@@ -1,17 +1,43 @@
 import events from '../../lib/events';
 
-const commands = {
-  voice: {
+const commands = [
+  {
     name: 'voice',
     description: 'Toggle voice commands.',
     hotkey: 'Alt+V'
   }
-};
+];
 
 const emitter = events.create();
 
 let listened = false;
 
+/**
+ * Handle keydown events.
+ * @param {KeyboardEvent} e
+ * @returns {void}
+ */
+const onKeydown = e => {
+  const ctrl = e.ctrlKey;
+  const alt = e.altKey;
+  const { key } = e;
+  let hotkey = '';
+
+  if (ctrl) hotkey += 'Ctrl+';
+  if (alt) hotkey += 'Alt+';
+  hotkey += key?.toLocaleUpperCase();
+
+  const command = commands.find(c => c.hotkey === hotkey);
+
+  if (!command) return;
+
+  emitter.emit('command', command);
+};
+
+/**
+ * Listen to commands.
+ * @returns {void}
+ */
 const listen = () => {
   if (listened) return;
 
@@ -19,27 +45,21 @@ const listen = () => {
 
   console.log('listening for commands');
 
-  document.addEventListener('keydown', e => {
-    console.log('keydown', e);
-    const ctrl = e.ctrlKey;
-    const alt = e.altKey;
-    const { key } = e;
-    let hotkey = '';
-
-    if (ctrl) hotkey += 'Ctrl+';
-    if (alt) hotkey += 'Alt+';
-    hotkey += key?.toLocaleUpperCase();
-
-    console.log('hotkey', hotkey);
-
-    const command = Object.keys(commands).find(
-      command => commands[command].hotkey === hotkey
-    );
-
-    if (!command) return;
-
-    emitter.emit('command', { command });
-  });
+  document.addEventListener('keydown', onKeydown);
 };
 
-export default { listen, ...emitter };
+/**
+ * Stop listening to commands.
+ * @returns {void}
+ */
+const stop = () => {
+  if (!listened) return;
+
+  listened = false;
+
+  console.log('stopped listening for commands');
+
+  document.removeEventListener('keydown', onKeydown);
+};
+
+export default { listen, stop, ...emitter };
--- chromium/extension/src/content/index.js
+++ chromium/extension/src/content/index.js
@@ -1,12 +1,24 @@
 import commands from './commands';
+import voice from './voice';
+
+/**
+ * Triggered when a command is received.
+ * @param {Command} command
+ */
+const onCommand = command => {
+  console.log('command received', command);
+
+  switch (command.name) {
+    case 'voice':
+      voice.ui.toggle();
+      break;
+  }
+};
 
 (() => {
-  console.log(commands);
-  commands.listen();
+  commands.on('command', onCommand);
 
-  commands.on('command', command => {
-    console.log('command activated', command);
-  });
+  commands.listen();
 
   console.log('content script loaded');
 })();
